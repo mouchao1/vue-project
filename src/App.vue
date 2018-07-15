70869ebc2ea0d8d519525c49046b0b7779cf2e0f
mouchao1 <41262631+mouchao1@users.noreply.github.com>
2018-07-15 22:22:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1 v-html="title"></h1>
+    <h2 v-text="msg"></h2>
+  </div> 
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '武汉理工大学信息管理与信息系统',
+      msg:'<王靖超 信管1603班>'
+    }
+  },
+  methods(){
+    
+  },
+  watch(){
+    
+  }
+}
+</script>
+
+<style>
+html{
+  height: 100%;
+}
+body{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+#app {
+  display: flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items:center;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
